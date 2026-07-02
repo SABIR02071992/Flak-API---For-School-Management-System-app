a30218ba82fe57e81f3d6d74a47e8799d2d14d11
Developer sabir <sabir.ansari@edugorilla.org>
2026-07-02 12:29:15 +0530

    Reset neon console password and .end file add

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-# from src import create_app
+from dotenv import load_dotenv
+import os
 
-# # हमारी नई Application Factory से ऐप लोड करें
-# app = create_app()
+# Load .env file
+load_dotenv()
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+# Optional: Check if .env loaded successfully
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 
-# app.py (F:\Dev-Sabir\Flask APIs\app.py)
 from src import create_app
 
-app = create_app('development')
+app = create_app("development")
 
-# 🔍 यह कोड टर्मिनल में प्रिंट करेगा कि आपकी असल में कौन-कौन सी APIs बनी हैं
 print("\n--- REGISTERED FLASK ROUTES ---")
 for rule in app.url_map.iter_rules():
     print(f"Path: {rule.rule} -> Methods: {list(rule.methods)}")
 print("-------------------------------\n")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
