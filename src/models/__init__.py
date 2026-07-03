e40d70e99dc1ac342c4f91d2f7ae289779636b2d
Developer sabir <sabir.ansari@edugorilla.org>
2026-07-03 09:47:36 +0530

    Fixes issue user login with correct role

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 from src.models.school_model import School
 from src.models.user_model import User
-from src.models.super_admin.super_admin_login_model import SuperAdmin
+from src.models.super_admin_login_model import SuperAdmin
 
 # ताकि अन्य फाइलें सीधे 'src.models' से इम्पोर्ट कर सकें
 __all__ = ['School', 'User', 'SuperAdmin']
